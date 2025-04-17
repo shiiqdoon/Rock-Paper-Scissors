256e10a8f38195cef96634b94bc49a5a80b1f523
shiiqdoon <shiiqdoon04@gmail.com>
2025-04-17 09:37:37 +0300

    Create a function thatbreturns computer choice javascript.js

---
+++ javascript.js
@@ -0,0 +1,22 @@
+// Step One
+
+const options = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice () {
+    let randomOption = Math.floor(Math.random()*options.length);
+    return options[randomOption];
+
+}
+console.log(getComputerChoice());
+
+
+
+// Step Two
+
+
+
+// Step Three
+
+
+// Step Four
+
